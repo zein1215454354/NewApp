ce011af4c4753a6d005b45b51dbd6bc0a3229860
zeinebmedallel <zeinebmeda97@gmail.com>
2022-01-03 18:31:46 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM debian:jessie-slim
+RUN apt-get update && apt-get install -y \
+    git \
+    python3 \
+    python3-pip
+RUN pip3 install Flask gunicorn
+ENV LC_ALL C.UTF-8
+ENV LANG C.UTF-8
+WORKDIR /app
+COPY . /app
---
+++ Web/app.py
@@ -0,0 +1,21 @@
+from flask import request, Flask
+import os
+import json
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World! "
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 9060))
+    app.run(host='0.0.0.0', port=port)
+
+@app.route('/plus_one')
+def plus_one():
+    x = int(request.args.get('x', 1))
+    return json.dumps({'x': x + 1})
+@app.route('/square')
+def square():
+    x = int(request.args.get('x', 1))
+    return json.dumps({'x': x * x})
