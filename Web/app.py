6bbda28357be0be9b6e0c64b9fe4f7d5ad2d53b4
zein1215454354 <82952150+zein1215454354@users.noreply.github.com>
2022-01-03 18:56:44 +0100

    Update app.py

--- Web/app.py
+++ Web/app.py
@@ -15,6 +15,10 @@ if __name__ == "__main__":
 def plus_one():
     x = int(request.args.get('x', 1))
     return json.dumps({'x': x + 1})
+@app.route('/plus_two')
+def plus_two():
+    x = int(request.args.get('x', 1))
+    return json.dumps({'x': x + 2})
 @app.route('/square')
 def square():
     x = int(request.args.get('x', 1))
